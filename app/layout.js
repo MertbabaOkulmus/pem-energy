d7ec3fce6bed5eba25b3d0c8e799fae4d002d4c8
Ayke-34 <aksoyaybuke12@gmail.com>
2024-10-06 10:06:22 +0300

    layout editing

--- app/layout.js
+++ app/layout.js
@@ -1,31 +1,47 @@
+"use client";
 import pemlogo from "./pemlogo.png";
 import Image from "next/image";
 import "./globals.css";
 import email from "./email.png";
 import phone from "./phone.png";
 import search from "./search.png";
-import { useEffect, useState } from "react/cjs/react.production.min";
 
-
+import React, { useEffect, useState } from "react";
 
 export default function RootLayout({ children }) {
-
-  const [layoutList,setLayoutList] = useState([]);
-   useEffect(()=>{
-    setLayoutList(['Hakkımızda','Haberler','Çözümler','Ürünler','Servis Destek','İletişim','Türkçe'])
-   },[])
+  const [layoutList, setLayoutList] = useState([]);
+  useEffect(() => {
+    setLayoutList([
+      "Anasayfa",
+      "Hakkımızda",
+      "Haberler",
+      "Çözümler",
+      "Ürünler",
+      "Servis Destek",
+      "İletişim",
+      "Türkçe",
+    ]);
+  }, []);
   return (
     <html lang="en">
       <body>
         <div className=" bg-black">
           <div className="text-white flex justify-between items-center px-4 mx-6 py-8">
             <div>
-              <a href="http://localhost:3000/"><Image height={80} width={300} src={pemlogo} /></a><br></br>
+              <a href="http://localhost:3000/">
+                <Image height={80} width={300} src={pemlogo} />
+              </a>
+              <br></br>
             </div>
 
             <div className="flex items-center">
-              <div className="flex items-center" >
-                <Image height={30} width={40} src={phone} className=" pr-2 py-2" />
+              <div className="flex items-center">
+                <Image
+                  height={30}
+                  width={40}
+                  src={phone}
+                  className=" pr-2 py-2"
+                />
                 <div>
                   <a href="tel:+90 216 576 66 60">+90 216 576 66 60</a>
                 </div>
@@ -35,26 +51,34 @@ export default function RootLayout({ children }) {
                 <div className="flex items-center">
                   <Image height={30} width={40} src={email} className=" pr-2" />
                   <div>
-                    <a href="mailto:satis@pemenerji.com.tr">satis@pemenerji.com.tr</a>
+                    <a href="mailto:satis@pemenerji.com.tr">
+                      satis@pemenerji.com.tr
+                    </a>
                   </div>
-
                 </div>
               </div>
             </div>
-            <div className="flex  items-end bg-black pr-2 mb-0 " >
-              <Image height={30} width={40} src={search} className="" alt="search" />
+            <div className="flex  items-end bg-black pr-2 mb-0 ">
+              <Image
+                height={30}
+                width={40}
+                src={search}
+                className=""
+                alt="search"
+              />
             </div>
           </div>
         </div>
         <div className="flex justify-around bg-orange-500 py-4">
-          <div className=" hover:text-white">Anasayfa </div>
-          <div>Hakkımızda</div>
-          <div>Haberler</div>
-          <div>Çözümler</div>
-          <div>Ürünler</div>
-          <div>Servis Destek</div>
-          <div>İletişim</div>
-          <div>Türkçe</div>
+          {layoutList.map((aybuke, index) => (
+            <>
+              <div className=" text-white hover:text-black text-base font-semibold">
+                {aybuke}
+              </div>
+              {layoutList.length-1!==index && (<div className="border-r-2"></div>)}
+             
+            </>
+          ))}
         </div>
         {children}
       </body>
