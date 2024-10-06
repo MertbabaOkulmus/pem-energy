1b9270d29e3b12c91733677319aaca299bb7fb8b
mertbabaokulmus <mertbaba.okulmus@hotmail.com>
2024-10-06 08:59:25 +0300

    layout

--- app/page.js
+++ app/page.js
@@ -2,14 +2,7 @@
 export default function Home() {
   return (
    <div>
-     <div>Anasayfa </div>
-     <div>Hakkımızda</div>
-     <div>Haberler</div>
-     <div>Çözümler</div>
-     <div>Ürünler</div>
-     <div>Servis Destek</div>
-     <div>İletişim</div>
-     <div>Türkçe</div>
+    
    </div>
   );
 }
